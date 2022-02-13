88531972f826163fa31596965f6ecbefe62af85c
Adham baquroun <44381355+ElNahoko@users.noreply.github.com>
2022-02-13 16:25:35 +0100

    Update WeatherForecastController.cs
    Added Hamza to

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace GED.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Hamza"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
