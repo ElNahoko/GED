1465f94cb4b5bbdb586dad5f8e70d28e8d17d53c
adham baquroun <adhamino@live.com>
2022-02-13 16:59:57 +0100

    Suggested changes

--- models/User.cs
+++ models/User.cs
@@ -4,6 +4,6 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public DateTime CreatedDate { get; set; } = DateTime.Now.ToUniversalTime();
     }
 }
